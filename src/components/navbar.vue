d5ef439fc3b71bb3860d4219034244df96f74038
Georges Ayeni <ayenigeorgepierre@gmail.com>
2022-02-04 18:49:16 +0100

    few mod

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -8,12 +8,12 @@
       <v-spacer></v-spacer>
       <v-btn
         depressed
-        class="white--text"
+        class="white--text text-capitalize"
         :ripple="{ class: 'red--text' }"
         @click="logout()"
       >
         <v-icon left>exit_to_app</v-icon>
-        log out
+        Logout
       </v-btn>
     </v-app-bar>
     <v-tab></v-tab>
@@ -29,39 +29,43 @@
     >
       <v-list>
         <v-list-item class="align-center justify-center">
-                <v-menu bottom min-width="200px" rounded offset-x color="brown">
-                  <template v-slot:activator="{ on }">
-                    <v-btn color="brown" icon x-large v-on="on">
-                      <v-avatar color="brown" size="40">
-                        <span class="white--text text-h6">{{ user.initials }}</span>
-                      </v-avatar>
-                    </v-btn>
-                  </template>
-                  <v-card>
-                    <v-list-item-content class="justify-center">
-                      <div class="mx-auto text-center">
-                        <v-avatar color="brown">
-                          <span class="white--text text-h5">{{ user.initials }}</span>
-                        </v-avatar>
-                        <h3>{{ user.fullName }}</h3>
-                        <p class="text-caption mt-1">
-                          {{ user.email }}
-                        </p>
-                        <v-divider class="my-3"></v-divider>
-                        <v-btn depressed rounded text> Edit Account </v-btn>
-                        <v-divider class="my-3"></v-divider>
-                        <v-btn depressed rounded text> Disconnect </v-btn>
-                      </div>
-                    </v-list-item-content>
-                  </v-card>
-                </v-menu>
+          <v-menu bottom min-width="200px" rounded offset-x color="brown">
+            <template v-slot:activator="{ on }">
+              <v-btn color="brown" icon x-large v-on="on">
+                <v-avatar color="brown" size="40">
+                  <span class="white--text text-h6">{{ user.initials }}</span>
+                </v-avatar>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-list-item-content class="justify-center">
+                <div class="mx-auto text-center">
+                  <v-avatar color="brown">
+                    <span class="white--text text-h5">{{ user.initials }}</span>
+                  </v-avatar>
+                  <h3>{{ user.fullName }}</h3>
+                  <p class="text-caption mt-1">
+                    {{ user.email }}
+                  </p>
+                  <v-divider class="my-3"></v-divider>
+                  <v-btn depressed rounded text> Edit Account </v-btn>
+                  <v-divider class="my-3"></v-divider>
+                  <v-btn depressed rounded text> Disconnect </v-btn>
+                </div>
+              </v-list-item-content>
+            </v-card>
+          </v-menu>
         </v-list-item>
+        <v-divider class="my-3"></v-divider>
+        <div >
         <v-list-item class="align-center justify-center">
           <v-icon large left>dashboard</v-icon>
           <v-list-content>
             <span>Hello world</span>
           </v-list-content>
         </v-list-item>
+        <v-divider class="my-3"></v-divider>
+        </div>
       </v-list>
     </v-navigation-drawer>
   </div>
@@ -72,12 +76,20 @@ export default {
     return {
       sidebar: true,
       hover: false,
+
       user: {
-        initials: 'JD',
-        fullName: 'John Doe',
-        email: 'john.doe@doe.com',
+        initials: "JD",
+        fullName: "John Doe",
+        email: "john.doe@doe.com",
       },
-      logout() {},
+      links: [
+        {content: "Home", icon: "home", road: "/home"},
+        {content: "Friends", icon: "multiple-avatar", road: "/home"},
+        
+        ],
+      methods: {
+        logout() {},
+      },
     };
   },
 };
