740b0be8e064e819449e0e3bfa68de23b8ee0944
Georges Ayeni <ayenigeorgepierre@gmail.com>
2022-02-09 16:38:47 +0100

    creation content friend

--- src/views/composantes/friends.vue
+++ src/views/composantes/friends.vue
@@ -1,21 +1,28 @@
 <template>
     <div>
-        <v-container>
+        <v-container class="mt-12">
             <v-row>
-                <v-col cols="12" sm="12" md="4" >
-                    <v-card dark class="text-center">
-                        <v-avatar>
-                            <img src="/favicon.ico" alt="friends pics">
+                <v-col cols="12" sm="12" md="4" v-for="friend in friends" :key="friend.name">
+                    <v-card dark class="text-center" max-width="300px">
+                        <v-avatar size="125">
+                            <v-img src="friend.url" alt="friends pics"> </v-img>
                         </v-avatar>
-                        <v-card-title>
-                            Friends.name
+                        <v-card-title class="display-1">
+                            {{ friend.name }}
                         </v-card-title>
-                        <v-card-subtitle>
-                        
+                        <v-card-subtitle class="text-left font-weight-italic">
+                            {{ friend.profession }}
                         </v-card-subtitle>
                         <v-card-content>
-
+                            {{ friend.bio }}
                         </v-card-content>
+                        <v-card-actions class="pa-5">
+                            <v-spacer></v-spacer>
+                            <v-btn depressed rounded class="grey--text" router to="/message">
+                                <v-icon small left> message </v-icon>
+                                Message
+                            </v-btn>
+                        </v-card-actions>
                     </v-card>
                 </v-col>
             </v-row>
@@ -26,8 +33,11 @@
 export default {
     data() {
         return {
-            friend: [
-                {name: "", profession: "", url: ""}
+            friends: [
+                {name: "Max Whitman", profession: "Web dev", url: "https://cdn.vuetifyjs.com/images/profiles/marcus.jpg", bio: "I'm dev web but never busy for you guys"},
+                {name: "Setephen Steel", profession: "Programmers", url: "https://avatars0.githubusercontent.com/u/9064066?v=4&s=460", bio: "I'm programmers so i've no life"},
+                {name: "Billy", profession: "Accountant", url: "https://cdn.vuetifyjs.com/images/cards/halcyon.png", bio: "Hey browsers"},
+                {name: "L'ami", profession: "Pentester", url: "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light", bio: "Hey"}
             ]
         }
     },
